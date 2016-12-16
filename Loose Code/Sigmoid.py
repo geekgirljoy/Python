813a1d047d48811cca728f409f1547cfd4b53930
Joy Harvel <geekgirl.joy@gmail.com>
2016-12-16 11:51:19 -0800

    Create Sigmoid.py

---
+++ Loose Code/Sigmoid.py
@@ -0,0 +1,15 @@
+# This algorithm computes a Sigmoid Curve
+# Then and outputs the computed range
+# For more information: https://en.wikipedia.org/wiki/Sigmoid_function
+
+import math;
+
+def sigmoid(t):
+    return 1 / (1 + math.exp(-t));
+
+current = -6.0;
+max = abs(current);
+
+while current <= max:
+    print(sigmoid(current));
+    current+=0.1;
