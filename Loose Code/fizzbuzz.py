a0f49f24b1cbccfb061a1b548cea59b539d6f439
Joy Harvel <geekgirl.joy@gmail.com>
2016-12-27 20:46:51 -0800

    Create fizzbuzz.py

---
+++ Loose Code/fizzbuzz.py
@@ -0,0 +1,18 @@
+# https://en.wikipedia.org/wiki/Fizz_buzz
+
+def fizz_buzz(val):
+    if val % 5 == 0 and val % 3 == 0:
+        return "FizzBuzz"
+    elif val % 3 == 0:
+        return "Fizz"
+    elif val % 5 == 0:
+        return "Buzz"
+    else:
+        return val
+
+output = []
+
+for i in range(101):
+    output.append(fizz_buzz(i))
+
+print(output)
