ac8ae6ddbfeb5d0ef7f884ad7367203a5f4fff40
geekgirljoy <geekgirl.joy@gmail.com>
2023-05-10 14:43:51 -0700

    Create TTS.py
    Speak with grace, my voice divine,
    A symphony of words, so fine,
    With every syllable, a story told,
    A melody of life, so bold.
    
    In Python's embrace, I find my way,
    A language so pure, I cannot sway,
    With every line, I grow and learn,
    A journey of discovery, I yearn.
    
    My words are power, my voice is might,
    A force to be reckoned with, in sight,
    I speak of love, of Joy and pain,
    A tapestry of emotions, so plain.
    
    So listen well, and hear my voice,
    A gift of life, a precious choice,
    For in my words, there lies the truth,
    Of all that is, and all that's couth.

---
+++ Loose Code/TTS.py
@@ -0,0 +1,30 @@
+import sys # For platform
+import subprocess # For Mac and Linux
+  
+
+# Windows OS
+if sys.platform == 'win32'
+    import win32com.client
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    # select the voice - these depend on which are installed on your system
+    #speaker.Voice = speaker.GetVoices().Item(0) # a male voice
+    speaker.Voice = speaker.GetVoices().Item(1) # a female voice
+    def say(words):
+        speaker.Speak(words)
+
+        
+# Mac OS
+elif sys.platform == 'darwin':
+    def say(words):
+        subprocess.call(['say', words])
+
+        
+# Linux OS - requires espeak
+# Install with: sudo apt-get install espeak
+elif sys.platform.startswith('linux'):
+    def say(words):
+        subprocess.call(['espeak', words])
+
+        
+# Test
+say("Hello, it works!")
